20a42ea5ae8df79ea23d285dbcc847d66b84674d
alekhya <alekhyauma99@gmail.com>
2023-10-12 16:48:55 +0530

    assi4

--- see.js
+++ see.js
@@ -1,25 +1,5 @@
-console.dir(document);
-console.log(document.URL);
-console.log(document.title);
-document.title = 123 ;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-var header = document.getElementById('main-header');
-header.style['border-bottom'] = "3px solid black";
-var title= document.getElementById('header-title');
-title.style.fontWeight = 'bold';
+document.addEventListener('DOMContentLoaded', function () {
+var titles = document.querySelectorAll('.title');
 
-var li = document.querySelectorAll('li');
-console.log(li);
-console.log(li[1]);
-li[1].textcontent = 'hello2';
-li[1].style.fontWeight='bold';
-li[1].style.backgroundColor = 'yellow';
-
-
-// Loop through the list items and apply styles
-for (var i = 0; i < li.length; i++) {
-  items[i].style.backgroundColor = '#f4f4f4';
-}
-
+console.log(titles);
+titles[0].textContent = 'Hello';
